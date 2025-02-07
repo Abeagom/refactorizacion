109f2a645071e4e807fad0870277058220cc2a85
Abeagom <beas.gomez.alberto@iescamas.es>
2025-02-08 00:34:51 +0100

    Añadidos tests de Employee

--- empleados/src/main/java/com/endes/Employee.java
+++ empleados/src/main/java/com/endes/Employee.java
@@ -9,6 +9,22 @@ public class Employee {
 		this.employeeYears=employeeYears;
 	}
 
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getEmployeeYears() {
+		return employeeYears;
+	}
+
+	public void setEmployeeYears(int employeeYears) {
+		this.employeeYears = employeeYears;
+	}
+
 	@Override
 	public String toString() {
 		return "[Name=" + name + ", EmployeeYears=" + employeeYears + "]";
